b2dfc5a03ba257760b4c6a61245f866d27bad0b3
Joel Frischknecht <joda.fri@hotmail.com>
2024-02-01 17:31:18 +0100

    Fix crossreferences to other two classes

--- docs/source/reporting.py
+++ docs/source/reporting.py
@@ -1,7 +1,7 @@
 class McfOptPolReport:
     """
-    Reporting tools for the :class:`ModifiedCausalForest` and
-    :class:`OptimalPolicy` classes.
+    Reporting tools for the :class:`~mcf_functions.ModifiedCausalForest` and
+    :class:`mcf_functions.OptimalPolicy` classes.
 
     Parameters
     ----------
