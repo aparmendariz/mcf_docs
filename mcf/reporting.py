8e4987e07f8c150a4c2d034750b1c28b6c9b8fb1
Ana Paula Armendariz Pacheco <143620833+aparmendariz@users.noreply.github.com>
2024-02-02 15:38:17 +0100

    adding clean reporting.py

--- mcf/reporting.py
+++ mcf/reporting.py
@@ -1,62 +1,41 @@
-"""Created on Fri Dec  8 13:42:19 2023.
-
-Contains classes and methods needed for mcf and optimal policy reporting.
-
-# -*- coding: utf-8 -*-
-
-@author: MLechner
-"""
-import datetime
-import os
-
-from mcf import reporting_functions as rep
-from mcf import mcf_general_sys as mcf_sys
-
-
 class McfOptPolReport:
-    """Methods and attributes for reporting mcf and optimal policy results.
+    """
+    .. versionadded:: 0.5.0
+        Provides reports about the main specification choices and most 
+        important results of the :class:`~mcf_functions.ModifiedCausalForest` and
+        :class:`~optpolicy_functions.OptimalPolicy` estimations.
 
-    Attributes
+    Parameters
     ----------
-        xxxx : Dictionary
-            Parameters to compute (partially) blinded IATEs.
-
-    Methods
-    -------
-        report : Constructing the report.
-        write : Write report to a txt-file.
-
+        mcf : Instance of the ModifiedCausalForest class or None, optional
+            Contains all information needed for reports. The default is None. 
+            
+        mcf_blind : Instance of the ModifiedCausalForest class or None, optional
+            Contains all information from blinded IATE analysis needed for
+            reports. The default is None.      
+            
+        mcf_sense : Instance of the ModifiedCausalForest class or None, optional
+            Contains all information from sensitivity analysis needed for
+            reports. The default is None.
+            
+        optpol : Instance of the OptimalPolicy class or None, optional
+            Contains all information from the optimal policy analysis needed
+            for reports. The default is None.
+            
+        outputpath : String or None, optional
+            Path to write the pdf file that is created with the :meth:`~McfOptPolReport.report`
+            method. If None, then an '/out' subdirectory of the current working
+            directory is used. If the latter does not exist, it is created.
+            
+        outputfile : String or None, optional
+            Name of the pdf file that is created by the :meth:`~McfOptPolReport.report` method.
+            If None, 'Reporting' is used as name. Any name will always appended
+            by string that contains the day and time (measured when the
+            programme ends).
     """
 
     def __init__(self, mcf=None, mcf_blind=None, mcf_sense=None, optpol=None,
                  outputpath=None, outputfile=None):
-        """
-        Define Constructor for ModifiedCausalForest class.
-
-        Args:
-        ----
-        mcf : Instance of the ModifiedCausalForest class or None, optional
-            Contains all information needed for reports. The default is None.
-        mcf_blind : Instance of the ModifiedCausalForest class or None, optional
-            Contains all information from blinded IATE analysis needed for
-            reports. The default is None.
-        mcf_sense : Instance of the ModifiedCausalForest class or None, optional
-            Contains all information from sensitivity analysis needed for
-            reports. The default is None.
-        optpol : Instance of the OptimalPolicy class or None, optional
-            Contains all information from the optimal policy analysis needed
-            for reports. The default is None.
-        outputpath : String or None, optional
-            Path to write the pdf file that is created with the reporting
-            method. If None, then an '/out' subdirectory of the current working
-            directory is used. If the latter does not exist, it is created.
-        outputfile : String or None, optional
-            Name of the pdf file that is created by the reporting method.
-            If None, 'Reporting' is used as name. Any name will always appended
-            by string that contains the day and time (measured when the
-            programme ends).
-
-        """
         self.gen_dict = gen_init(outputfile, outputpath)
         self.mcf_o, self.opt_o, self.sens_o = mcf, optpol, mcf_sense
         self.blind_o = mcf_blind
@@ -65,9 +44,10 @@ class McfOptPolReport:
         self.sens = self.sens_o is not None
         self.blind = self.blind_o is not None
         self.text = {}
-
+    
     def report(self):
-        """Create the content of the report.
+        """Create a PDF report using instances of the :class:`~mcf_functions.ModifiedCausalForest` and 
+        :class:`~optpolicy_functions.OptimalPolicy` classes and saves the file to a user provided location.
 
         Returns
         -------
