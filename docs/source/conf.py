851695d52505cb1d10586e096fd031d8878f672a
Ana Paula Armendariz Pacheco <143620833+aparmendariz@users.noreply.github.com>
2024-02-02 15:27:07 +0100

    sys.path.insert(0, os.path.abspath('../../mcf'))  conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,8 +13,8 @@ import os
 import sys
 # sys.path.insert(0, os.path.abspath('..'))
 # sys.path.insert(0, os.path.abspath('.'))
-sys.path.insert(0, os.path.abspath('../..'))
-#sys.path.insert(0, os.path.abspath('../../mcf'))
+# sys.path.insert(0, os.path.abspath('../..'))
+sys.path.insert(0, os.path.abspath('../../mcf'))
 
 # -- Project information -----------------------------------------------------
 
