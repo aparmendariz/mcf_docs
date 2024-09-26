2f860d9078e65b9c50cdc3bcf2ca0886d9329a47
Ana Paula Armendariz Pacheco <143620833+aparmendariz@users.noreply.github.com>
2024-09-26 11:54:31 +0200

    Update optpolicy_functions.py

--- mcf/optpolicy_functions.py
+++ mcf/optpolicy_functions.py
@@ -50,7 +50,8 @@ class OptimalPolicy:
         Default (or None) is False.
 
     fair_material_disc_method : String (or None), optional
-        Method on how to perform the discretization for materially relevant features.
+        Method on how to perform the discretization for materially relevant
+        features.
        'NoDiscretization' : Variables are not changed. If one of the features
         has more different values than FAIR_MATERIAL_MAX_GROUPS, all
         materially relevant features will formally be treated as continuous.
