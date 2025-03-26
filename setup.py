09fedf43701d4bd72d2484361a129327e8a6367d
Linda Fiorina Odermatt <118910345+FiorinaLi@users.noreply.github.com>
2025-03-26 13:45:12 +0100

    Update setup.py 072

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
 import os
 
 def read(fname):
@@ -7,27 +7,31 @@ def read(fname):
 setup(
   name = 'mcf',
   packages = ['mcf'],
-  version = '0.4.3',
+  version = '0.7.2',
   license='MIT',
-  description = 'mcf is a powerful package to estimate heterogeneous treatment effects for multiple treatment models in a selection-on-observables setting and learn optimal policy rules',
+  description = 'The Python package mcf implements the Modified Causal Forest introduced by Lechner (2018). This package allows you to estimate heterogeneous treatment effects for binary and multiple treatments from experimental or observational data. Additionally, mcf offers the capability to learn optimal policy allocations.',
   author = 'mlechner',
   author_email = 'michael.lechner@unisg.ch',
-  url = 'https://github.com/MCFpy/mcf',
+  url = 'https://mcfpy.github.io/mcf/#/',
   keywords = ['causal machine learning, heterogeneous treatment effects, causal forests, optimal policy learning'],
   long_description=read('README.txt'),
   classifiers=[
     'Development Status :: 5 - Production/Stable',
     'Topic :: Software Development :: Build Tools',
     'License :: OSI Approved :: MIT License',
-    'Programming Language :: Python :: 3.11'
+    'Programming Language :: Python :: 3.12'
   ],
   install_requires=[
-    'ray>=2.8.1',
-     'pandas>=2.1.4',
-     'matplotlib>=3.8.2',
-     'numba>=0.58.1',
-     'sympy>=1.12',
-     'scikit-learn>=1.3.2',
-     'scipy>=1.11.4'
+    'ray>=2.36.0',
+     'pandas>=2.2.2',
+     'matplotlib>=3.9.2',
+     'numba>=0.60.0',
+     'sympy>=1.13.3',
+     'scikit-learn>=1.5.2',
+     'scipy>=1.14.1',
+     'torch>=2.4.1',
+     'fpdf2>=2.7.9', 
+     'seaborn>=0.13.2', 
+     'statsmodels>=0.14.4'
      ]
 )
